fdaaad8714c46aab95d7227131bb8d33451d0610
oldsheeep3 <js2oyb@gmail.com>
2026-07-02 21:33:13 +0900

    update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const a = document.querySelectorAll('h2');
-
-console.log(a);
-console.warn('aの中身を確認する');
-
-for (let i = 0; i < a.length; i++) {
-    a[i].innerText = (i + 1) + '番目のh2です';
+function name(a, b) {
+    alert(a + b);
 }
+
+const a = document.getElementsByClassName("subtitle");
+const elem = a[0];
+
+elem.style.color = "red";
+elem.style.fontSize = "30px";
+
+map
+foreach
+
+document.addEventListener
+
+
